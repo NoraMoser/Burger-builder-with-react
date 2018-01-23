8ef225d5c8e86e53b0b0b8f50f223fa71f646bfa
Nora Moser <noramoser@Noras-MBP.hsd1.tn.comcast.net>
2018-01-23 14:13:28 -0600

    added message and css and dynamic view starting

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,11 +6,17 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
 
     //this turns it into an array so we can use the .map method on it- then we're trying to make an array that's the length of the values from the original state.
-    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_ , i) => {
             return <BurgerIngredient key={igKey + i} type={igKey} />;
         });
-    });
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>please add ingredients!</p>
+    }
     return (
         //the classes.Burger is the css for the entire burger (size, etc) and the BurgerIngredient it's bringing in css from the BurgerIngredient css.  Type is the case names.
         <div className={classes.Burger}>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,16 +7,17 @@ class BurgerBuilder extends Component {
     //using a state is how we're going to make it dynamic
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 2,
-            cheese: 3, 
-            meat: 1
+            salad: 0,
+            bacon: 0,
+            cheese: 0, 
+            meat: 0
         }
     }
 
     //this exports content from Burger.js and then is able to be used on app.js
     render () {
         return (
+            //the ingredients on here refer to the ingredients in the state.  this is how the burger.js has a variable to relate to and how it knows the length that it needs to be.
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
