875b4d5d7a683f7654c4c16ae19a52b12b744963
Nora Moser <noralmoser@gmail.com>
2018-01-29 10:29:24 -0600

    user can dynamically add ingredients

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -9,8 +9,9 @@ const buildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
         <button className={classes.Less}>Less</button>
-        <button className={classes.More}>More</button>
+        <button className={classes.More} onClick={props.added}>More</button>
     </div>
+    //added property comes from BuildControls.js which is what ties in the add Ingredients function from BurgerBuilder.js
 );
 
 export default buildControl;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,14 @@ import Burger from '../../components/Burger/Burger';
 
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+//global variables are typically all caps
+const INGREDIENT_PRICES = {
+    salad: 0.2,
+    bacon: 0.8,
+    cheese: 1.0,
+    meat: 2.3
+}
+
 class BurgerBuilder extends Component {
 
     //using a state is how we're going to make it dynamic
@@ -13,7 +21,8 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0, 
             meat: 0
-        }
+        },
+        totalPrice: 4
     }
 
     addIngredientHandler = (type) => {
@@ -26,6 +35,13 @@ class BurgerBuilder extends Component {
         };
         //this makes the new count go to the state
         updatedIngredients[type] = newCount;
+        //(below) this adds the prices listed in the global variable to each ingredient added with type
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        //this adds the prices to the overall price for a plain burger which I set to 4.
+        const newPrice = oldPrice + priceAddition;
+
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
     }
 
     removeIngredientHandler = (type) => {
@@ -38,7 +54,7 @@ class BurgerBuilder extends Component {
             //the ingredients on here refer to the ingredients in the state.  this is how the burger.js has a variable to relate to and how it knows the length that it needs to be.
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <BuildControls />
+                <BuildControls addIngredients={this.addIngredientHandler}/>
             </Aux>
         );
     }
