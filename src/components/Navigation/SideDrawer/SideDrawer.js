67d5f77a8de2e2061a76cfda336e3f40a09d99d7
Nora Moser <noralmoser@gmail.com>
2018-02-05 13:26:48 -0600

    starting to make it mobile friendly

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+
+const sideDrawer = (props) => {
+    //make it an object instead of returning just jsx when you want to do more stuff
+    // ...
+    return (
+        <div className={classes.SideDrawer}>
+            <Logo />
+            <nav>
+                <NavigationItems />
+            </nav>
+        </div>
+    );
+};
+
+export default sideDrawer;
