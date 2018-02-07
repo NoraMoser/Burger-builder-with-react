a0927ac64f1ef21ada42db3acfdb3768c2320f82
Nora Moser <noralmoser@gmail.com>
2018-02-07 13:37:38 -0600

    routes are set up now and starting to use them in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -8,10 +10,11 @@ class App extends Component {
   render() {
     return (
       //layout is the nav bar and burger builder is all the content being created.
+      //exact prevents / from becomming a prefix and therefore appearing with every route.
       <div>
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/" exact component={BurgerBuilder} />
         </Layout>
       </div>
     );
