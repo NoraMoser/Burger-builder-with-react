ca8225c90a7adf5a473523fee121475f02dbe04e
Nora Moser <noralmoser@gmail.com>
2018-02-08 15:00:00 -0600

    holy crap redux is actually working

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,26 +9,12 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import { connect } from 'react-redux';
 import * as actionTypes from '../../store/actions';
 
-//global variables are typically all caps
-const INGREDIENT_PRICES = {
-    salad: 0.2,
-    bacon: 0.8,
-    cheese: 1.0,
-    meat: 2.3
-}
+
 
 class BurgerBuilder extends Component {
 
-    //using a state is how we're going to make it dynamic
     
     state = {
-        // ingredients: {
-        //     salad: 0,
-        //     bacon: 0,
-        //     cheese: 0, 
-        //     meat: 0
-        // },
-        totalPrice: 4,
         purchasable: false,
         purchasing: false
     }
@@ -47,49 +33,7 @@ class BurgerBuilder extends Component {
         this.setState({purchasable: sum > 0})
     }
 
-    addIngredientHandler = (type) => {
-        const oldCount = this.state.ingredients[type];
-        const newCount = oldCount + 1;
-
-        //this makes us able to update the state
-        const updatedIngredients = {
-            ...this.state.ingredients
-        };
-        //this makes the new count go to the state
-        updatedIngredients[type] = newCount;
-        //(below) this adds the prices listed in the global variable to each ingredient added with type
-        const priceAddition = INGREDIENT_PRICES[type];
-        const oldPrice = this.state.totalPrice;
-        //this adds the prices to the overall price for a plain burger which I set to 4.
-        const newPrice = oldPrice + priceAddition;
-
-        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
-        this.updatePurchasableProduct(updatedIngredients);
-    }
-
-    removeIngredientHandler = (type) => {
-        const oldCount = this.state.ingredients[type];
-        //throws an error if we go below 0 without the if statement so with this nothing will happen if it's below 0
-        if(oldCount <= 0) {
-            return;
-        }
-        //taking away an ingredient now
-        const newCount = oldCount - 1;
-
-        //this makes us able to update the state
-        const updatedIngredients = {
-            ...this.state.ingredients
-        };
-        //this makes the new count go to the state
-        updatedIngredients[type] = newCount;
-        const priceDeduction = INGREDIENT_PRICES[type];
-        const oldPrice = this.state.totalPrice;
-        //this takes away the prices of each ingredient we click less for.
-        const newPrice = oldPrice - priceDeduction;
-
-        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
-        this.updatePurchasableProduct(updatedIngredients);
-    }
+    
 
     purchaseHandler = () => {
         this.setState({purchasing: true});
@@ -135,7 +79,7 @@ class BurgerBuilder extends Component {
                     ingredients={this.props.ings}
                     purchaseCancelled={this.purchaseCancelHandler}
                     purchaseContinued={this.purchaseContinueHandler} 
-                    price={this.state.totalPrice}/>
+                    price={this.props.price}/>
                 </Modal>
                 <Burger ingredients={this.props.ings}/>
                 <BuildControls 
@@ -144,7 +88,7 @@ class BurgerBuilder extends Component {
                 disabled={disabledInfo}
                 purchasable={this.state.purchasable}
                 ordered={this.purchaseHandler}
-                price={this.state.totalPrice}/>
+                price={this.props.price}/>
             
             </Aux>
         );
@@ -153,7 +97,8 @@ class BurgerBuilder extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients
+        ings: state.ingredients,
+        price: state.totalPrice
     };
 }
 
